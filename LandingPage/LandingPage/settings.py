fc0c8b5594b02c97f69cc8ce9be5f6519ed3983a
SergeyK1983 <ssp-serg@yandex.ru>
2023-11-12 13:06:20 +0300

    settings_templates

--- LandingPage/LandingPage/settings.py
+++ LandingPage/LandingPage/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'LandingPage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
