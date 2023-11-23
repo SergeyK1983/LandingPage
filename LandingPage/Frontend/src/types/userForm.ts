a2a93b64543cb118850b11030eda417f986d939d
ArkadySkrynnikov <catmaster1488@gmail.com>
2023-11-23 14:00:42 +0700

    types

--- LandingPage/Frontend/src/types/userForm.ts
+++
@@ -1,8 +0,0 @@
-export interface IUserForm {
-    userName: string;
-    userAge: number;
-    userProfession: string;
-    userExperience: number;
-    userSalary: number;
-    userContact: string;
-}
